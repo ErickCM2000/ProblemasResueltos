a7b4297b0ed3438152b0a06e09b105a96c177f0a
Erick Castañeda Martínez <erickce40@gmail.com>
2019-12-10 12:39:56 -0600

    archivos agregados

---
+++ Codeforces/266B-codeforces.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+#include "stack"
+
+using namespace std;
+
+int main (){
+
+    stack mystack<int>;
+
+    mystack.push(10);
+    mystack.push(100);
+    mystack.push(1000);
+
+    return 0;
+}
